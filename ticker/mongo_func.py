5d300daa683c09179cfb2f2a0052ae4c73e56219
Daniel Grafstr�m <daniel.grafstrom@mycronic.com>
2019-12-01 20:16:03 +0100

    fixed bug with settings

--- ticker/mongo_func.py
+++ ticker/mongo_func.py
@@ -11,7 +11,7 @@ client:MongoClient
 def init_mongodb():
     global client
     client = MongoClient(host='userver2', retryWrites=True)
-    #mongoengine.register_connection(alias='settings', name='apps_settings', host='userver2', port=27017)
+    mongoengine.register_connection(alias='settings', name='apps_settings', host='userver2', port=27017)
     #mongoengine.register_connection(alias='NewsDb', name='ticker_db', host='userver2', port=27017)
     #mongoengine.register_connection(alias='ticker', name='ticker_db', host='userver2', port=27017)
 
