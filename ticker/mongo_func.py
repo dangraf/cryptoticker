8793d792a7bd9ec064da347f1091f9d5e7fa4a46
Daniel Grafstr�m <daniel.grafstrom@mycronic.com>
2019-12-02 22:21:20 +0100

    fixed bug with news alias for mongodb

--- ticker/mongo_func.py
+++ ticker/mongo_func.py
@@ -12,7 +12,7 @@ def init_mongodb():
     global client
     client = MongoClient(host='userver2', retryWrites=True)
     mongoengine.register_connection(alias='settings', name='apps_settings', host='userver2', port=27017)
-    #mongoengine.register_connection(alias='NewsDb', name='ticker_db', host='userver2', port=27017)
+    mongoengine.register_connection(alias='NewsDb', name='ticker2_db', host='userver2', port=27017)
     #mongoengine.register_connection(alias='ticker', name='ticker_db', host='userver2', port=27017)
 
 def save_tickerdata2(data:Dict, collection_name: str):
